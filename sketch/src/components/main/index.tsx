8febfe04cc5e8df3c44dff1e3423f5ebb6554896
Vaishnavi-Manogaran <85192082+Vaishnavi-Manogaran@users.noreply.github.com>
2022-10-16 20:26:36 +0530

    base environment

---
+++ sketch/src/components/main/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './main.module.scss';
+
+const Main=()=>{
+  return (
+    <div className={classes.main}>
+      check
+    </div>
+  );
+}
+
+export default Main;
