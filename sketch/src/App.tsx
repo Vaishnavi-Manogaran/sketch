7e8794e19dbd58a8a6bf3342496d1d10f7e59ff3
Vaishnavi-Manogaran <85192082+Vaishnavi-Manogaran@users.noreply.github.com>
2022-11-07 15:00:15 +0530

    sketch - normal format

--- sketch/src/App.tsx
+++ sketch/src/App.tsx
@@ -1,16 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
-import Main from './components/main';
+import React from "react";
+import "./App.scss";
+import Main from "./components/main";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Main />
-      </header>
-    </div>
-  );
+  return <Main />;
 }
 
 export default App;
